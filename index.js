d2ca81cc26be51e8a86aa6a4eb95ed72bfb8cf1e
Mohammed Elalj <simo@elalj.com>
2017-01-08 15:47:35 -0800

    expressMiddleware + lint

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 'use strict';
 
-module.exports = {
-  starter: require('./lib/starter'),
-  logger: require('./lib/logger'),
-  utils: require('./lib/utils'),
-  gcloud: require('./lib/gcloud'),
-  cache: require('./lib/cache'),
-  publisher: require('./lib/publisher'),
-};
+// Nothing here. Everything is in ./lib
+module.exports = {};
